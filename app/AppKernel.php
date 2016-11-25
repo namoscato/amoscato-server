619f49e2173a51581b846deaf4245a4a797b6c4e
Nick Amoscato <nicholas@amoscato.com>
2016-11-24 23:17:37 -0500

    Bump dependencies; remove unused dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Amoscato\Bundle\AppBundle\AppBundle(),
             new Amoscato\Bundle\IntegrationBundle\IntegrationBundle(),
