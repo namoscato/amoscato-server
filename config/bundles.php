46f8fb27cf58dfa58077cf786071217b791cc879
Nick Amoscato <nick@amoscato.com>
2025-01-01 13:36:03 -0500

    Remove SensioFrameworkExtraBundle
    in favor of built-in Symfony PHP attributes

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,5 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
